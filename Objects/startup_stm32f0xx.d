38b3c8b90c8116ab51947e8b891bb43e683b7589
nkstark <nkstark413@gmail.com>
2015-11-09 21:45:01 +0100

    Signed-off-by: nkstark <nkstark413@gmail.com>

--- Objects/startup_stm32f0xx.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f0xx.o: cmsis\Source\Templates\arm\startup_stm32f0xx.s
--- Objects/system_stm32f0xx_1.d
+++
@@ -1,7 +0,0 @@
-.\objects\system_stm32f0xx_1.o: user\system_stm32f0xx.c
-.\objects\system_stm32f0xx_1.o: .\cmsis\Include\stm32f0xx.h
-.\objects\system_stm32f0xx_1.o: .\cmsis\core_cm0.h
-.\objects\system_stm32f0xx_1.o: e:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_stm32f0xx_1.o: .\cmsis\core_cmInstr.h
-.\objects\system_stm32f0xx_1.o: .\cmsis\core_cmFunc.h
-.\objects\system_stm32f0xx_1.o: .\cmsis\Include\system_stm32f0xx.h
